1cefb520134842d1187a612b9996962bdff252dd
xuxiubin <1174495446@qq.com>
2022-08-22 20:21:55 +0800

    调整图片布局

--- assets/md/material/basic/Image.md
+++ assets/md/material/basic/Image.md
@@ -1,7 +1,15 @@
-```widget
-imageDemo
+```widgetsRow
+imageDemo1,imageDemo2
 ```
 
-```widget
-iconDemo
+```widgetsRow
+imageDemo3,imageDemo4,imageDemo5
+```
+
+```widgetsRow
+imageDemo6,imageDemo7,imageDemo8
+```
+
+```widgetsRow
+iconDemo1,iconDemo2
 ```
--- lib/demo/material/basic/Image.demo.dart
+++ lib/demo/material/basic/Image.demo.dart
@@ -17,372 +17,283 @@ class MyIcons{
 
 registImageDemo(BuildContext context) {
 
-  var imageDemo = Demo(
-    'imageDemo',
+  var imageDemo1 = Demo(
+    "imageDemo1",
+    const DecoratedBox(
+      decoration: BoxDecoration(color: Colors.blue),
+      child: Padding(
+        padding: EdgeInsets.all(20),
+          child: Image(
+            image: AssetImage("logo.png"),
+            width: 100,
+          ),
+      )
+    ),
+    r'''
+    Image(
+      image: AssetImage("logo.png"),
+      width: 100,
+    )
+    ''',
+    '本地图片',
+  );
+
+  var imageDemo2 = Demo(
+    "imageDemo2",
+    DecoratedBox(
+        decoration: const BoxDecoration(color: Colors.blue),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Image.network(
+            "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
+            width: 100.0,
+          ),
+        )
+    ),
+    r'''
+    Image.network(
+      "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
+      width: 100.0,
+    )
+    ''',
+    '网络图片',
+  );
+
+  var imageDemo3 = Demo(
+    "imageDemo3",
     DecoratedBox(
         decoration: const BoxDecoration(color: Colors.blue),
         child: Padding(
             padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    const Image(
-                      image: AssetImage("logo.png"),
-                      width: 100,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 10),
-                      child: Image.network(
-                        "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
-                        width: 100.0,
-                      ),
-                    )
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 40),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            'BoxFit.fill',
-                            style: TextStyle(
-                              color: Colors.white
-                            ),
-                          ),
-                          Image(
-                            image: AssetImage("logo.png"),
-                            width: 100,
-                            height: 50,
-                            fit: BoxFit.fill,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            'BoxFit.cover',
-                            style: TextStyle(
-                                color: Colors.white
-                            ),
-                          ),
-                          Image(
-                            image: AssetImage("logo.png"),
-                            width: 100,
-                            height: 50,
-                            fit: BoxFit.cover,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            '[default]BoxFit.contain',
-                            style: TextStyle(
-                                color: Colors.white
-                            ),
-                          ),
-                          Image(
-                            image: AssetImage("logo.png"),
-                            width: 100,
-                            height: 50,
-                            fit: BoxFit.contain,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            'BoxFit.fitHeight',
-                            style: TextStyle(
-                                color: Colors.white
-                            ),
-                          ),
-                          Image(
-                            image: AssetImage("logo.png"),
-                            width: 100,
-                            height: 50,
-                            fit: BoxFit.fitHeight,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            'BoxFit.fitWidth',
-                            style: TextStyle(
-                                color: Colors.white
-                            ),
-                          ),
-                          Image(
-                            image: AssetImage("logo.png"),
-                            width: 100,
-                            height: 50,
-                            fit: BoxFit.fitWidth,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            'BoxFit.none',
-                            style: TextStyle(
-                                color: Colors.white
-                            ),
-                          ),
-                          Image(
-                            image: AssetImage("logo.png"),
-                            width: 100,
-                            height: 50,
-                            fit: BoxFit.none,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              ],
+            child: Container(
+              color: Colors.red,
+              width: 100,
+              height: 50,
+              child: const Image(
+                image: AssetImage("logo.png"),
+                fit: BoxFit.fill,
+              ),
             )
         )
     ),
     r'''
+    Container(
+      color: Colors.red,
+      width: 100,
+      height: 50,
+      child: const Image(
+        image: AssetImage("logo.png"),
+        fit: BoxFit.fill,
+      ),
+    )
+    ''',
+    '填满、变形',
+  );
+
+  var imageDemo4 = Demo(
+    "imageDemo4",
     DecoratedBox(
         decoration: const BoxDecoration(color: Colors.blue),
         child: Padding(
             padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    const Image(
-                      image: AssetImage("logo.png"),
-                      width: 100,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 10),
-                      child: Image.network(
-                        "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
-                        width: 100.0,
-                      ),
-                    )
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 40),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            'BoxFit.fill',
-                            style: TextStyle(
-                              color: Colors.white
-                            ),
-                          ),
-                          Image(
-                            image: AssetImage("logo.png"),
-                            width: 100,
-                            height: 50,
-                            fit: BoxFit.fill,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            'BoxFit.cover',
-                            style: TextStyle(
-                                color: Colors.white
-                            ),
-                          ),
-                          Image(
-                            image: AssetImage("logo.png"),
-                            width: 100,
-                            height: 50,
-                            fit: BoxFit.cover,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            '[default]BoxFit.contain',
-                            style: TextStyle(
-                                color: Colors.white
-                            ),
-                          ),
-                          Image(
-                            image: AssetImage("logo.png"),
-                            width: 100,
-                            height: 50,
-                            fit: BoxFit.contain,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            'BoxFit.fitHeight',
-                            style: TextStyle(
-                                color: Colors.white
-                            ),
-                          ),
-                          Image(
-                            image: AssetImage("logo.png"),
-                            width: 100,
-                            height: 50,
-                            fit: BoxFit.fitHeight,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            'BoxFit.fitWidth',
-                            style: TextStyle(
-                                color: Colors.white
-                            ),
-                          ),
-                          Image(
-                            image: AssetImage("logo.png"),
-                            width: 100,
-                            height: 50,
-                            fit: BoxFit.fitWidth,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            'BoxFit.none',
-                            style: TextStyle(
-                                color: Colors.white
-                            ),
-                          ),
-                          Image(
-                            image: AssetImage("logo.png"),
-                            width: 100,
-                            height: 50,
-                            fit: BoxFit.none,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              ],
+            child: Container(
+              color: Colors.red,
+              width: 100,
+              height: 50,
+              child: const Image(
+                image: AssetImage("logo.png"),
+                fit: BoxFit.cover,
+              ),
             )
+        )
+    ),
+    r'''
+    Container(
+      color: Colors.red,
+      width: 100,
+      height: 50,
+      child: const Image(
+        image: AssetImage("logo.png"),
+        fit: BoxFit.cover,
+      ),
+    )
+    ''',
+    '比例填满、裁剪',
+  );
+
+  var imageDemo5 = Demo(
+    "imageDemo5",
+    DecoratedBox(
+        decoration: const BoxDecoration(color: Colors.blue),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            color: Colors.red,
+            width: 100,
+            height: 50,
+            child: const Image(
+              image: AssetImage("logo.png"),
+              fit: BoxFit.contain,
+            ),
+          )
+        )
+    ),
+    r'''
+    Container(
+      color: Colors.red,
+      width: 100,
+      height: 50,
+      child: const Image(
+        image: AssetImage("logo.png"),
+        fit: BoxFit.contain,
+      ),
+    )
+    ''',
+    '默认、比例缩放',
+  );
+
+  var imageDemo6 = Demo(
+    "imageDemo6",
+    DecoratedBox(
+        decoration: const BoxDecoration(color: Colors.blue),
+        child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Container(
+              color: Colors.red,
+              width: 100,
+              height: 50,
+              child: const Image(
+                image: AssetImage("logo.png"),
+                fit: BoxFit.fitHeight,
+              ),
+            )
+        )
+    ),
+    r'''
+    Container(
+      color: Colors.red,
+      width: 100,
+      height: 50,
+      child: const Image(
+        image: AssetImage("logo.png"),
+        fit: BoxFit.fitHeight,
+      ),
+    )
+    ''',
+    '高度、比例、裁剪',
+  );
+
+  var imageDemo7 = Demo(
+    "imageDemo7",
+    DecoratedBox(
+        decoration: const BoxDecoration(color: Colors.blue),
+        child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Container(
+              color: Colors.red,
+              width: 100,
+              height: 50,
+              child: const Image(
+                image: AssetImage("logo.png"),
+                fit: BoxFit.fitWidth,
+              ),
+            )
+        )
+    ),
+    r'''
+    Container(
+      color: Colors.red,
+      width: 100,
+      height: 50,
+      child: const Image(
+        image: AssetImage("logo.png"),
+        fit: BoxFit.fitWidth,
+      ),
+    )
+    ''',
+    '宽度、比例、裁剪',
+  );
+
+  var imageDemo8 = Demo(
+    "imageDemo8",
+    DecoratedBox(
+        decoration: const BoxDecoration(color: Colors.blue),
+        child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Container(
+              color: Colors.red,
+              width: 100,
+              height: 50,
+              child: const Image(
+                image: AssetImage("logo.png"),
+                fit: BoxFit.none,
+              ),
+            )
+        )
+    ),
+    r'''
+    Container(
+      color: Colors.red,
+      width: 100,
+      height: 50,
+      child: const Image(
+        image: AssetImage("logo.png"),
+        fit: BoxFit.none,
+      ),
+    )
+    ''',
+    '图片实际大小，裁剪',
+  );
+
+
+  var iconDemo1 = Demo(
+    "iconDemo1",
+    const DecoratedBox(
+        decoration: BoxDecoration(color: Colors.blue),
+        child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Icon(Icons.accessible,color: Colors.white),
+        )
+    ),
+    r'''
+    const DecoratedBox(
+        decoration: BoxDecoration(color: Colors.blue),
+        child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Icon(Icons.accessible,color: Colors.white),
         )
     ),
     ''',
-    '图片',
+    '系统icon',
   );
 
-  var iconDemo = Demo(
-    'iconDemo',
-    DecoratedBox(
-        decoration: const BoxDecoration(color: Colors.blue),
+  var iconDemo2 = Demo(
+    "iconDemo2",
+    const DecoratedBox(
+        decoration: BoxDecoration(color: Colors.blue),
         child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Column(
-                  children: const [
-                    Text(
-                      '系统',
-                      style: TextStyle(
-                          color: Colors.white
-                      ),
-                    ),
-                    Icon(Icons.accessible,color: Colors.white),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Column(
-                    children: const [
-                      Text(
-                        '自定义',
-                        style: TextStyle(
-                            color: Colors.white
-                        ),
-                      ),
-                      Icon(MyIcons.book,color: Colors.white),
-                    ],
-                  ),
-                )
-              ],
-            )
+          padding: EdgeInsets.all(20),
+          child: Icon(MyIcons.book,color: Colors.white),
         )
     ),
     r'''
-    
-    //pubspec.yaml
-    fonts:
-    - family: myIcon  #指定一个字体名
-      fonts:
-        - asset: assets/fonts/iconfont.ttf
-    
     class MyIcons{
+      // book 图标
       static const IconData book = const IconData(
           0xe6b8,
           fontFamily: 'myIcon',
           matchTextDirection: true
       );
     }
-    
-    DecoratedBox(
-        decoration: const BoxDecoration(color: Colors.blue),
+
+    const DecoratedBox(
+        decoration: BoxDecoration(color: Colors.blue),
         child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Column(
-                  children: const [
-                    Text(
-                      '系统',
-                      style: TextStyle(
-                          color: Colors.white
-                      ),
-                    ),
-                    Icon(Icons.accessible,color: Colors.white),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Column(
-                    children: const [
-                      Text(
-                        '自定义',
-                        style: TextStyle(
-                            color: Colors.white
-                        ),
-                      ),
-                      Icon(MyIcons.book,color: Colors.white),
-                    ],
-                  ),
-                )
-              ],
-            )
+          padding: EdgeInsets.all(20),
+          child: Icon(MyIcons.book,color: Colors.white),
         )
     ),
     ''',
-    'Icon',
+    '自定义icon',
   );
 }
